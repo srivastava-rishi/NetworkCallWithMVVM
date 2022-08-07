3ec10a249d19a9ed140bbd402f20276350ac5878
Rishi Srivastava <official.rishisrivastava@example.com>
2022-08-08 00:53:50 +0530

    viewmodel made

--- app/src/main/java/com/rsstudio/networkcalling/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/rsstudio/networkcalling/viewmodel/MainViewModel.kt
@@ -1,4 +1,20 @@
 package com.rsstudio.networkcalling.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.rsstudio.networkcalling.api.BeerInfoNetworkEntity
+import com.rsstudio.networkcalling.api.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MainViewModel
+@Inject
+constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    private val _response = MutableLiveData<List<BeerInfoNetworkEntity>>()
+
 }
